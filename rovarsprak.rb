705d8e866c48db41506d20d20e9d0744504ed50f
Jimmy Börjesson <lagginglion@gmail.com>
2014-04-27 18:33:39 +0200

    Wrote the CLI

--- rovarsprak.rb
+++ rovarsprak.rb
@@ -17,3 +17,29 @@ class RovarSprak
   end
 
 end
+
+class CLI
+  def initialize(argv)
+    @argv = argv
+  end
+
+  def text
+    @argv[1..@argv.size].join(' ')
+  end
+
+  def direction
+    if @argv[0] =~ /to|from/
+      @argv[0]
+    else
+      raise 'you must specify wether to translate "to" or "from" as the first argument'
+    end
+  end
+
+  def output
+    RovarSprak.send(direction, text)
+  end
+end
+
+if __FILE__ == $0
+  puts CLI.new(ARGV).output
+end
--- rovarsprak_test.rb
+++ rovarsprak_test.rb
@@ -10,3 +10,42 @@ describe RovarSprak do
     RovarSprak.from('rorononjoja rorövovarordodotottoteror').must_equal 'ronja rövardotter'
   end
 end
+
+describe CLI do
+
+  describe 'to' do
+    let(:input) { %w( to ronja rövardotter ) }
+    subject { CLI.new(input) }
+
+    it 'has the text' do
+      subject.text.must_equal 'ronja rövardotter'
+    end
+
+    it 'has the direction to' do
+      subject.direction.must_equal 'to'
+    end
+
+    it 'has the correct output' do
+      subject.output.must_equal 'rorononjoja rorövovarordodotottoteror'
+    end
+  end
+
+  describe 'from' do
+    let(:input) { %w( from rorononjoja rorövovarordodotottoteror ) }
+    subject { CLI.new(input) }
+
+    it 'has the direction from' do
+      subject.direction.must_equal 'from'
+    end
+
+    it 'has the correct output' do
+      subject.output.must_equal 'ronja rövardotter'
+    end
+
+  end
+
+  it 'it throws on invalid translation direction' do
+    -> { CLI.new(%w{wrong ronja rövardotter}).direction }.must_raise RuntimeError
+  end
+
+end
